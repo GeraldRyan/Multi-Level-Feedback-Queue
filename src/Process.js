624e3643a6292d6bef0dd4be1d87f93219c9f37e
GeraldRyan <gerald.ryan40@yahoo.com>
2020-12-11 22:47:57 -0600

    started working on process class

--- src/Process.js
+++ src/Process.js
@@ -1,4 +1,4 @@
-const { SchedulerInterrupt } = require('./constants/index');
+const { SchedulerInterrupt } = require("./constants/index");
 
 // A class representation of a process that may be blocking
 // or non-blocking. We can specify how much CPU time a process
@@ -6,54 +6,60 @@ const { SchedulerInterrupt } = require('./constants/index');
 // is blocking; if so, the amount of blocking time needed is
 // randomly determined.
 class Process {
-    constructor(pid, cpuTimeNeeded=null, blocking=false) {
-        this._pid = pid;
-        this.queue = null;
-        this.cpuTimeNeeded = (cpuTimeNeeded !== null) ? cpuTimeNeeded : Math.round(Math.random() * 1000);
-        this.blockingTimeNeeded = blocking ? Math.round(Math.random() * 100) : 0;
-        // A bool representing whether this process was toggled from blocking to non-blocking or vice versa
-        this.stateChanged = false;
-    }
-    
-    setParentQueue(queue) {
-        // TODO 
-    }
+  constructor(pid, cpuTimeNeeded = null, blocking = false) {
+    this._pid = pid;
+    this.queue = null;
+    this.cpuTimeNeeded =
+      cpuTimeNeeded !== null ? cpuTimeNeeded : Math.round(Math.random() * 1000);
+    this.blockingTimeNeeded = blocking ? Math.round(Math.random() * 100) : 0;
+    // A bool representing whether this process was toggled from blocking to non-blocking or vice versa
+    this.stateChanged = false;
+  }
 
-    isFinished() {
+  setParentQueue(queue) {
+    // is this a string? An object of a class? What type is this argument?
+    // TODO
 
-    }
+    // GCR says: Done or more needed?
+    this.queue = queue;
+  }
 
-    // If no blocking time is needed by this process, decrement the amount of 
+  isFinished() {
+    // GCR: What is this method supposed to be doing??
+    // GCR: Delete refernence? Or is a bool to check if it's finished?
+  }
+
+  executeProcess(time) {
+    // If no blocking time is needed by this process, decrement the amount of
     // CPU time it needs by the input time
+    if (this.blockingTimeNeeded === 0) {
+      this.cpuTimeNeeded -= time;
+    }
     // If blocking time is needed by this process, move it to the blocking queue
-    // by emitting the appropriate interrupt
-    // Make sure the `stateChanged` flag is toggled appropriately
-    executeProcess(time) {
-
-   }
-
-   // If this process requires blocking time, decrement the amount of blocking
-   // time it needs by the input time
-   // Once it no longer needs to perform any blocking execution, move it to the 
-   // top running queue by emitting the appropriate interrupt
-   // Make sure the `stateChanged` flag is toggled appropriately
-    executeBlockingProcess(time) {
-
+    else { // blocking time required 
+      // by emitting the appropriate interrupt
+      console.log("Emit my appropriate interrupt");
+      // Make sure the `stateChanged` flag is toggled appropriately
+      this.stateChanged = true;
     }
+  }
 
-    // Returns this process's stateChanged property
-    isStateChanged() {
+  // If this process requires blocking time, decrement the amount of blocking
+  // time it needs by the input time
+  // Once it no longer needs to perform any blocking execution, move it to the
+  // top running queue by emitting the appropriate interrupt
+  // Make sure the `stateChanged` flag is toggled appropriately
+  executeBlockingProcess(time) {}
 
-    }
+  // Returns this process's stateChanged property
+  isStateChanged() {}
 
-    get pid() {
+  get pid() {}
 
-    }
-
-    // Private function used for testing; DO NOT MODIFY
-    _getParentQueue() {
-        return this.queue;
-    }
+  // Private function used for testing; DO NOT MODIFY
+  _getParentQueue() {
+    return this.queue;
+  }
 }
 
 module.exports = Process;
